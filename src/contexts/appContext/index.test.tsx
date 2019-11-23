4f7898c23f35c29fc45c4edc23d7d11544e548cd
PiotrAleksander <piotrhugonow@gmail.com>
2019-11-23 17:07:23 +0000

    WIP context v2

--- src/contexts/appContext/index.test.tsx
+++ src/contexts/appContext/index.test.tsx
@@ -5,8 +5,6 @@ import "@testing-library/jest-dom/extend-expect";
 import AppContext, { AppProvider, AppConsumer } from ".";
 
 test("AppContext return default value", () => {
-  const { getByText } = render(<AppConsumer>
-    {}
-  </AppConsumer>)
-  expect(getByText(/^My Name Is:/)).toHaveTextContent('My Name Is: Unknown'
+  const { getByText } = render(<AppConsumer>{}</AppConsumer>);
+  expect(getByText(/^My Name Is:/)).toHaveTextContent("My Name Is: Unknown");
 });
